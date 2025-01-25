f2c1e4f0f738d33b66340e6fa166e4e79e5a6034
Leon Ruggiero <lruggiero@becomposable.com>
2025-01-25 07:23:15 +0000

    update textOptionsFallback

--- core/src/options.ts
+++ core/src/options.ts
@@ -16,13 +16,25 @@ export interface TextFallbackOptions {
 export const textOptionsFallback: ModelOptionsInfo = {
     _options_id: "text-fallback",
     options: [
-        { name: "max_tokens", type: OptionType.numeric, min: 1, max: 4096, default: 128, description: "The maximum number of tokens to generate" },
-        { name: "temperature", type: OptionType.numeric, min: 0, max: 1, default: 1, description: "The temperature of the generated image" },
+        {
+            name: "max_tokens", type: OptionType.numeric, min: 1, max: 8192, default: 512,
+            integer: true, description: "The maximum number of tokens to generate"
+        },
+        {
+            name: "temperature", type: OptionType.numeric, min: 0.0, max: 1.0, default: 0.7,
+            integer: false, step: 0.1, description: "The temperature of the generated image"
+        },
         { name: "top_p", type: OptionType.numeric, min: 0, max: 1, default: 1, description: "The nucleus sampling probability of the generated image" },
         { name: "top_k", type: OptionType.numeric, min: 0, max: 1024, default: 50, description: "The top k sampling of the generated image" },
-        { name: "presence_penalty", type: OptionType.numeric, min: 0, max: 1, default: 0, description: "The presence penalty of the generated image" },
-        { name: "frequency_penalty", type: OptionType.numeric, min: 0, max: 1, default: 0, description: "The frequency penalty of the generated image" },
-        { name: "stop_sequence", type: OptionType.string_list, value: [""], description: "The stop sequence of the generated image" },
+        {
+            name: "presence_penalty", type: OptionType.numeric, min: -2.0, max: -2.0, default: 0,
+            integer: false, step: 0.1, description: "The presence penalty of the generated image"
+        },
+        {
+            name: "frequency_penalty", type: OptionType.numeric, min: -2.0, max: -2.0, default: 0,
+            integer: false, step: 0.1, description: "The frequency penalty of the generated image"
+        },
+        { name: "stop_sequence", type: OptionType.string_list, value: [], description: "The stop sequence of the generated image" },
     ]
 };
 
