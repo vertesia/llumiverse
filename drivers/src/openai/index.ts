1fe7276fe8bb563bbadfe80fc4fcf3a9e688020b
Leon Ruggiero <lruggiero@becomposable.com>
2025-04-14 11:18:45 +0100

    update wordBlacklist filter

--- drivers/src/openai/index.ts
+++ drivers/src/openai/index.ts
@@ -284,9 +284,12 @@ export abstract class BaseOpenAIDriver extends AbstractDriver<
             return (m as any)?.capabilities?.chat_completion ?? false
         });
 
+        //Some of these use the completions API instead of the chat completions API
+        //Others are for non-text input modalities. Update as required.
+        const wordBlacklist = ["embed", "whisper", "dall-e", "babbage", "davinci", "transcribe", "audio", "moderation", "tts"];
         //OpenAI has very little information, filtering based on name.
         result = result.filter((m) => {
-            return !m.id.includes("embed") && !m.id.includes("dall-e") && !m.id.includes("whisper");
+            return !wordBlacklist.some((word) => m.id.includes(word));
         });
 
         const models = filter ? result.filter(filter) : result;
