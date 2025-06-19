d77c07ecd69698ead7ec34def60ba9f48085fdf4
Leon Ruggiero <lruggiero@becomposable.com>
2025-06-19 11:35:47 +0100

    initial refactor

--- drivers/src/vertexai/models/claude.ts
+++ drivers/src/vertexai/models/claude.ts
@@ -1,14 +1,13 @@
-import * as AnthropicAPI from '@anthropic-ai/sdk';
-import { ContentBlock, ContentBlockParam, ImageBlockParam, Message, TextBlockParam } from "@anthropic-ai/sdk/resources/index.js";
+import { ContentBlock, ContentBlockParam, DocumentBlockParam, ImageBlockParam, Message, MessageParam, TextBlockParam, ToolResultBlockParam } from "@anthropic-ai/sdk/resources/index.js";
 import {
-    AIModel, Completion, CompletionChunkObject, ExecutionOptions, JSONObject, ModelType,
-    PromptOptions, PromptRole, PromptSegment, readStreamAsBase64, ToolUse, VertexAIClaudeOptions
+    AIModel, Completion, CompletionChunkObject, ExecutionOptions, getMaxTokensLimitVertexAi, JSONObject, ModelType,
+    PromptRole, PromptSegment, readStreamAsBase64, StatelessExecutionOptions, ToolUse, VertexAIClaudeOptions
 } from "@llumiverse/core";
 import { asyncMap } from "@llumiverse/core/async";
 import { VertexAIDriver } from "../index.js";
 import { ModelDefinition } from "../models.js";
-
-type MessageParam = AnthropicAPI.Anthropic.MessageParam;
+import { MessageCreateParamsBase, MessageCreateParamsNonStreaming, RawMessageStreamEvent } from "@anthropic-ai/sdk/resources/messages.js";
+import { MessageStreamParams } from "@anthropic-ai/sdk/resources/index.mjs";
 
 interface ClaudePrompt {
     messages: MessageParam[];
@@ -35,18 +34,23 @@ function collectTextParts(content: any) {
     return out.join('\n');
 }
 
-function maxToken(max_tokens: number | undefined, model: string): number {
-    const contains = (str: string, substr: string) => str.indexOf(substr) !== -1;
-    if (max_tokens) {
-        return max_tokens;
-    } else if (contains(model, "claude-3-5")) {
-        return 8192;
+function maxToken(option: StatelessExecutionOptions): number {
+    const modelOptions = option.model_options as VertexAIClaudeOptions | undefined;
+    if (modelOptions && typeof modelOptions.max_tokens === "number") {
+        return modelOptions.max_tokens;
     } else {
-        return 4096
+        // Fallback to the default max tokens limit for the model
+        if (option.model.includes('claude-3-7-sonnet')) {
+            return 64000; // Claude 3.7 can go up to 128k with a beta header, but when no max tokens is specified, we default to 64k.
+        }
+        return getMaxTokensLimitVertexAi(option.model);
     }
 }
 
-async function collectImageBlocks(segment: PromptSegment, contentBlocks: ContentBlockParam[]) {
+async function collectFileBlocks(segment: PromptSegment, contentBlocks: ContentBlockParam[], restrictedTypes: boolean = false): Promise<void> {
+    // When restrictedTypes is true, we only allow text files and images.
+    // i.e. output is Array<TextBlockParam | ImageBlockParam>
+    
     for (const file of segment.files || []) {
         if (file.mime_type?.startsWith("image/")) {
             const allowedTypes = ["image/png", "image/jpeg", "image/gif", "image/webp"];
@@ -62,16 +66,18 @@ async function collectImageBlocks(segment: PromptSegment, contentBlocks: Content
                     data: await readStreamAsBase64(await file.getStream()),
                     media_type: mimeType
                 }
-            });
-        } else if (file.mime_type?.startsWith("text/")) {
-            contentBlocks.push({
-                source: {
-                    type: 'text',
-                    data: await readStreamAsBase64(await file.getStream()),
-                    media_type: 'text/plain'
-                },
-                type: 'document'
-            });
+            } satisfies ImageBlockParam);
+        } else if (!restrictedTypes) {
+            if (file.mime_type?.startsWith("text/")) {
+                contentBlocks.push({
+                    source: {
+                        type: 'text',
+                        data: await readStreamAsBase64(await file.getStream()),
+                        media_type: 'text/plain'
+                    },
+                    type: 'document'
+                } satisfies DocumentBlockParam);
+            }
         }
     }
 }
@@ -87,36 +93,37 @@ export class ClaudeModelDefinition implements ModelDefinition<ClaudePrompt> {
             provider: 'vertexai',
             type: ModelType.Text,
             can_stream: true,
-        } as AIModel;
+        } satisfies AIModel;
     }
 
-    async createPrompt(_driver: VertexAIDriver, segments: PromptSegment[], options: PromptOptions): Promise<ClaudePrompt> {
+    async createPrompt(_driver: VertexAIDriver, segments: PromptSegment[], options: ExecutionOptions): Promise<ClaudePrompt> {
         // Convert the prompt to the format expected by the Claude API
-        const systemSegments: TextBlockParam[] = segments
+        const system: TextBlockParam[] = segments
             .filter(segment => segment.role === PromptRole.system)
             .map(segment => ({
                 text: segment.content,
                 type: 'text'
             }));
 
-        const safetySegments: TextBlockParam[] = segments
-            .filter(segment => segment.role === PromptRole.safety)
-            .map(segment => ({
-                text: segment.content,
-                type: 'text'
-            }));
-
         if (options.result_schema) {
+            let schemaText: string = '';
+            if (options.tools && options.tools.length > 0) {
+                schemaText = "When not calling tools, the answer must be a JSON object using the following JSON Schema:\n" + JSON.stringify(options.result_schema);
+            } else {
+                schemaText = "The answer must be a JSON object using the following JSON Schema:\n" + JSON.stringify(options.result_schema);
+            }
+
             const schemaSegments: TextBlockParam = {
-                text: "The answer must be a JSON object using the following JSON Schema:\n" + JSON.stringify(options.result_schema),
+                text: schemaText,
                 type: 'text'
             }
-            safetySegments.push(schemaSegments);
+            system.push(schemaSegments);
         }
 
-        const messages: MessageParam[] = [];
+        let messages: MessageParam[] = [];
+        const safetyMessages: MessageParam[] = [];
         for (const segment of segments) {
-            if (segment.role === PromptRole.system || segment.role === PromptRole.safety) {
+            if (segment.role === PromptRole.system) {
                 continue;
             }
 
@@ -125,38 +132,58 @@ export class ClaudeModelDefinition implements ModelDefinition<ClaudePrompt> {
                     throw new Error("Tool prompt segment must have a tool use ID");
                 }
 
-                const imageBlocks: ImageBlockParam[] = [];
-                await collectImageBlocks(segment, imageBlocks);
+                const contentBlocks: Array<TextBlockParam | ImageBlockParam> = [];
+
+                if (segment.content) {
+                    contentBlocks.push({
+                        type: 'text',
+                        text: segment.content
+                    } satisfies TextBlockParam);
+                }
+                
+                await collectFileBlocks(segment, contentBlocks, true);
 
                 messages.push({
                     role: 'user',
                     content: [{
                         type: 'tool_result',
                         tool_use_id: segment.tool_use_id,
-                        content: [{
-                            type: 'text',
-                            text: segment.content || ''
-                        }, ...imageBlocks]
-                    }]
+                        content: contentBlocks,
+                    } satisfies ToolResultBlockParam]
                 });
 
             } else {
                 const contentBlocks: ContentBlockParam[] = [];
-                collectImageBlocks(segment, contentBlocks);
+                
                 if (segment.content) {
                     contentBlocks.push({
                         type: 'text',
                         text: segment.content
+                    } satisfies TextBlockParam);
+                }
+
+                await collectFileBlocks(segment, contentBlocks);
+
+                if (contentBlocks.length === 0) {
+                    continue; // skip empty segments
+                }
+
+                if (segment.role === PromptRole.safety) {
+                    safetyMessages.push({
+                        role: 'user',
+                        content: contentBlocks
+                    });
+                } else {
+                    messages.push({
+                        role: segment.role === PromptRole.assistant ? 'assistant' : 'user',
+                        content: contentBlocks
                     });
                 }
-                messages.push({
-                    role: segment.role === PromptRole.assistant ? 'assistant' : 'user',
-                    content: contentBlocks
-                });
+                
             }
         }
 
-        const system = systemSegments.concat(safetySegments);
+        messages = messages.concat(safetyMessages);
 
         return {
             messages: messages,
@@ -166,9 +193,6 @@ export class ClaudeModelDefinition implements ModelDefinition<ClaudePrompt> {
 
     async requestTextCompletion(driver: VertexAIDriver, prompt: ClaudePrompt, options: ExecutionOptions): Promise<Completion> {
         const client = driver.getAnthropicClient();
-        const splits = options.model.split("/");
-        const modelName = splits[splits.length - 1];
-        options = { ...options, model: modelName };
         options.model_options = options.model_options as VertexAIClaudeOptions;
 
         if (options.model_options?._option_id !== "vertexai-claude") {
@@ -177,23 +201,11 @@ export class ClaudeModelDefinition implements ModelDefinition<ClaudePrompt> {
 
         let conversation = updateConversation(options.conversation as ClaudePrompt, prompt);
 
-        const result = await client.messages.create({
-            ...conversation, // messages, system,
-            tools: options.tools, // we are using the same shape as claude for tools
-            temperature: options.model_options?.temperature,
-            model: modelName,
-            max_tokens: maxToken(options.model_options?.max_tokens, modelName),
-            top_p: options.model_options?.top_p,
-            top_k: options.model_options?.top_k,
-            stop_sequences: options.model_options?.stop_sequence,
-            thinking: options.model_options?.thinking_mode ?
-                {
-                    budget_tokens: options.model_options?.thinking_budget_tokens ?? 1024,
-                    type: "enabled"
-                } : {
-                    type: "disabled"
-                }
-        }) as Message;
+        const { payload, requestOptions } = getClaudePayload(options, conversation);
+        // disable streaming, the create function is overloaded so payload type matters.
+        const nonStreamingPayload: MessageCreateParamsNonStreaming = { ...payload, stream: false };
+
+        const result = await client.messages.create(nonStreamingPayload, requestOptions) satisfies Message;
 
         const text = collectTextParts(result.content);
         const tool_use = collectTools(result.content);
@@ -201,7 +213,6 @@ export class ClaudeModelDefinition implements ModelDefinition<ClaudePrompt> {
         conversation = updateConversation(conversation, createPromptFromResponse(result));
 
         return {
-            chat: [prompt, { role: result.role, content: result.content }],
             result: text ?? '',
             tool_use,
             token_usage: {
@@ -212,51 +223,72 @@ export class ClaudeModelDefinition implements ModelDefinition<ClaudePrompt> {
             // make sure we set finish_reason to the correct value (claude is normally setting this by itself)
             finish_reason: tool_use ? "tool_use" : claudeFinishReason(result?.stop_reason ?? ''),
             conversation
-        } as Completion;
+        } satisfies Completion;
     }
 
     async requestTextCompletionStream(driver: VertexAIDriver, prompt: ClaudePrompt, options: ExecutionOptions): Promise<AsyncIterable<CompletionChunkObject>> {
         const client = driver.getAnthropicClient();
-        const splits = options.model.split("/");
-        const modelName = splits[splits.length - 1];
-        options = { ...options, model: modelName };
-        options.model_options = options.model_options as VertexAIClaudeOptions;
+        const model_options = options.model_options as VertexAIClaudeOptions | undefined;
 
-        if (options.model_options?._option_id !== "vertexai-claude") {
+        if (model_options?._option_id !== "vertexai-claude") {
             driver.logger.warn("Invalid model options", { options: options.model_options });
         }
 
-        const response_stream = await client.messages.stream({
-            ...prompt, // messages, system,
-            tools: options.tools, // we are using the same shape as claude for tools
-            temperature: options.model_options?.temperature,
-            model: modelName,
-            max_tokens: maxToken(options.model_options?.max_tokens, modelName),
-            top_p: options.model_options?.top_p,
-            top_k: options.model_options?.top_k,
-            stop_sequences: options.model_options?.stop_sequence,
-            thinking: options.model_options?.thinking_mode ?
-                {
-                    budget_tokens: options.model_options?.thinking_budget_tokens ?? 1024,
-                    type: "enabled"
-                } : {
-                    type: "disabled"
-                }
-        });
+        const { payload, requestOptions } = getClaudePayload(options, prompt);
+        const streamingPayload: MessageStreamParams = { ...payload, stream: true };
 
-        const stream = asyncMap(response_stream, async (item: any) => {
-            if (item.type == "message_start") {
-                return {
-                    result: '',
-                    token_usage: { prompt: item?.message?.usage?.input_tokens, result: item?.message?.usage?.output_tokens },
-                    finish_reason: undefined,
-                }
+        const response_stream = await client.messages.stream(streamingPayload, requestOptions);
+
+        const stream = asyncMap(response_stream, async (streamEvent: RawMessageStreamEvent) => {
+            console.log(JSON.stringify(streamEvent, null, 2));
+            switch (streamEvent.type) {
+                case "message_start":
+                    return {
+                        result: '',
+                        token_usage: {
+                            prompt: streamEvent.message.usage.input_tokens,
+                            result: streamEvent.message.usage.output_tokens
+                        }
+                    } satisfies CompletionChunkObject;
+
+                case "content_block_delta":
+                    // Handle different delta types
+                    switch (streamEvent.delta.type) {
+                        case "text_delta":
+                            return {
+                                result: streamEvent.delta.text ?? ''
+                            } satisfies CompletionChunkObject;
+                        case "thinking_delta":
+                            if (model_options?.include_thoughts) {
+                                return {
+                                    result: streamEvent.delta.thinking ?? '',
+                                } satisfies CompletionChunkObject;
+                            }
+                            break;
+                        case "signature_delta":
+                            // Signature deltas, signify the end of the thoughts.
+                            if (model_options?.include_thoughts) {
+                                return {
+                                    result: '\n\n', // Double newline for more spacing
+                                } satisfies CompletionChunkObject;
+                            }
+                            break;
+                    }
+                    break;
+                case "message_delta":
+                    return {
+                        result: '',
+                        token_usage: {
+                            result: streamEvent.usage.output_tokens
+                        },
+                        finish_reason: claudeFinishReason(streamEvent.delta.stop_reason ?? undefined),
+                    } satisfies CompletionChunkObject;
             }
+
+            // Default case for all other event types
             return {
-                result: item?.delta?.text ?? '',
-                token_usage: { result: item?.usage?.output_tokens },
-                finish_reason: claudeFinishReason(item?.delta?.stop_reason ?? ''),
-            }
+                result: ''
+            } satisfies CompletionChunkObject;
         });
 
         return stream;
@@ -302,4 +334,44 @@ function updateConversation(conversation: ClaudePrompt | undefined | null, promp
         messages: baseMessages.concat(prompt.messages || []),
         system: baseSystemMessages.concat(prompt.system || [])
     };
+}
+interface RequestOptions {
+    headers?: Record<string, string>;
+}
+
+function getClaudePayload(options: ExecutionOptions, prompt: ClaudePrompt): { payload: MessageCreateParamsBase, requestOptions: RequestOptions | undefined} {
+    const splits = options.model.split("/");
+    const modelName = splits[splits.length - 1];
+    const model_options = options.model_options as VertexAIClaudeOptions;
+
+    // Add beta header for Claude 3.7 models to enable 128k output tokens
+    let requestOptions: RequestOptions | undefined = undefined;
+    if (modelName.includes('claude-3-7-sonnet') && (model_options?.max_tokens ?? 0) > 64000) {
+        requestOptions = {
+            headers: {
+                'anthropic-beta': 'output-128k-2025-02-19'
+            }
+        };
+    }
+
+    const payload = {
+        messages: prompt.messages,
+        system: prompt.system,
+        tools: options.tools, // we are using the same shape as claude for tools
+        temperature: model_options?.temperature,
+        model: modelName,
+        max_tokens: maxToken(options),
+        top_p: model_options?.top_p,
+        top_k: model_options?.top_k,
+        stop_sequences: model_options?.stop_sequence,
+        thinking: model_options?.thinking_mode ?
+            {
+                budget_tokens: model_options?.thinking_budget_tokens ?? 1024,
+                type: "enabled" as const
+            } : {
+                type: "disabled" as const
+            }
+    };
+
+    return { payload, requestOptions };
 }
