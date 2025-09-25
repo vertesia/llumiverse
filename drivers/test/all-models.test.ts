9eb49b471c3d7da42875d52c8155954d78b3a4a5
Leon Ruggiero <lruggiero@vertesiahq.com>
2025-09-25 09:18:50 +0100

    revert 001 removal for gemini

--- drivers/test/all-models.test.ts
+++ drivers/test/all-models.test.ts
@@ -27,7 +27,7 @@ if (process.env.GOOGLE_PROJECT_ID && process.env.GOOGLE_REGION) {
             region: process.env.GOOGLE_REGION as string,
         }),
         models: [
-            "publishers/google/models/gemini-2.0-flash-lite",
+            "publishers/google/models/gemini-2.0-flash-lite-001",
            // "gemini-1.5-flash", //legacy id format
             "publishers/anthropic/models/claude-3-7-sonnet",
             "publishers/meta/models/llama-4-scout-17b-16e-instruct-maas"
