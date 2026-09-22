a170bb8243d50afc2ccea11b877615c8562beb5f
Leon Ruggiero <lruggiero@vertesiahq.com>
2026-09-22 19:47:49 +0100

    test: include gpt audio options in schema order

--- common/src/schemas/model-options.test.ts
+++ common/src/schemas/model-options.test.ts
@@ -109,6 +109,7 @@ describe('ModelOptionsSchema', () => {
             'OpenAiGptImageOptions',
             'OpenAiTranscriptionOptions',
             'OpenAiSpeechOptions',
+            'OpenAiAudioOptions',
             'XAIGrokImageOptions',
             'GroqOptions',
             'MistralTextOptions',
